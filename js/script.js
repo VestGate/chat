46898641b7b57319c36ef26160cadc38570f5db0
VestGate <33828348+VestGate@users.noreply.github.com>
2018-04-27 11:37:34 +0530

    Batch update

---
+++ js/script.js
@@ -0,0 +1,3 @@
+
+function add_chatinline(){var hccid=26548718;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
+add_chatinline(); 
